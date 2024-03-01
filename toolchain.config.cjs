8cfa2244a745a99cd42a0d9d00ebcd434456df24
Andy Edwards <jedwards@fastmail.com>
2024-02-29 19:20:44 -0600

    chore: fix pretest script

--- toolchain.config.cjs
+++ toolchain.config.cjs
@@ -1,8 +1,20 @@
 /* eslint-env node, es2018 */
+
+const path = require('path')
+const execa = require('@jcoreio/toolchain/util/execa.cjs')
+const defaultenv = require('defaultenv')
+
 module.exports = {
   cjsBabelEnv: { forceAllTransforms: true },
   outputEsm: false,
   scripts: {
+    pretest: {
+      run: async () => {
+        defaultenv([path.resolve('env/local.js')])
+        if (!process.env.CI) await execa('docker', ['compose', 'up', '-d'])
+      },
+      description: 'runs before test',
+    },
     postbuild:
       'mkdir -p dist/migrations && cp -vp src/migrations/*.sql dist/migrations',
   },
